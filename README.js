e5688a4fe14f20d563425cc0fb6ec4d98e1483e2
webkiso <40767048+webkiso@users.noreply.github.com>
2018-07-08 17:11:58 +0900

    Update README.js

--- README.js
+++ README.js
@@ -12324,7 +12324,7 @@ phina.namespace(function() {
           playButton: {
             className: 'phina.ui.Button',
             arguments: [{
-              text: '▶',
+              text: '👎',
               width: 128,
               height: 128,
               fontColor: params.fontColor,
